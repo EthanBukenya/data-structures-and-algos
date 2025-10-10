314ffc9edef9d490bcd01d86e4537ed4b18b82ac
EthanBukenya <ethancylb@gmail.com>
2025-10-10 12:08:46 +0300

    initial commit

---
+++ BigOnotation.py
@@ -0,0 +1,12 @@
+# Big O notation
+# This the measurement of how running time and space requirement complexity of an algorithm grows as as imput size increases.
+
+# data structures
+# These are containers storing data in a specific memory layout
+
+# Arrays ara data structures were elements are stored in a contiguous memory location.
+# A dictionary is a Hash Table/Map that stores data using a key and value format and it's complexity is order of 1 ie O(1) since searching through is a constant.
+# it uses a key on which You apply a hash function to access a specific memory address that is connected to a given element.
+
+# Algorithm
+# This is a set of programmed instructions to accomplish a task
