0015225de3ab170f36921356db7f13a8022ba734
EthanBukenya <ethancylb@gmail.com>
2025-10-10 12:30:26 +0300

    initial commit

--- BigOnotation.py
+++ BigOnotation.py
@@ -10,3 +10,21 @@
 
 # Algorithm
 # This is a set of programmed instructions to accomplish a task
+
+# Big O examples
+''' def foo(arr) size(arr) -> 100 : 0.23 milliseconds
+                           -> 1000: 2.30 milliseconds  
+                          Note: time ideally depends on how fast or slow your computer is!
+
+    so big O notation is a mathematical notation for measuring time independent of the computer hardware
+        time = a*n + b
+
+        1: keep the fastest growing term
+        time = a*n
+
+        2: drop the constants
+        time = n
+
+        = O(n) , Therefore the Big O time Complexity is Order of n
+        
+ '''
