b5fd429838342007713b40aafefc6a988ce73c4f
EthanBukenya <ethancylb@gmail.com>
2025-10-10 15:28:16 +0300

    Big O notation conclusion

--- BigOnotation.py
+++ BigOnotation.py
@@ -1,5 +1,6 @@
 # Big O notation
 # This the measurement of how running time and space requirement complexity of an algorithm grows as as imput size increases.
+# we usually assume the worst case senario to be solved, ie searching for a number at the end of the last indexes in list
 
 # data structures
 # These are containers storing data in a specific memory layout
@@ -44,5 +45,47 @@ def squared_numbers(numbers):
 
 numbers = [2, 3, 4, 5]
 print(squared_numbers(numbers))
+
+'''
+# Order of 1 or O(1) time complexity example
+# time = a * n + b
+'''
+def find_first_pe(prices, eps, index):
+    pe= prices[index]/eps[index]
+    return pe
+'''
+
+# Order of n^2 time complexity
+# time = a * n^2 + b
+''' 
+numbers = [1, 4, 7, 3, 5, 3, 6, 7, 8, 9, 10]
+
+for i in range(len(numbers)):
+    for j in range(i+1, len(numbers)):
+        if numbers[i] == numbers[j]:
+            print("the duplicates are ", numbers[i])
+            break
+'''
+# finding position of a number 77 in a list
+# the simple primitive approach O(n)
+# because it uses n number of iterations yet binary search takes 3 iterations thus O(log n)
+# incase if in interview: O(n) seems slow, can you optimize O(log n)
+numbers = [1, 3, 7, 2, 5, 77, 9, 0, 10]
+numbers.sort()
+print(numbers)
+
+for i in range(len(numbers)):
+    if numbers[i] == 77:
+        print(i)
+
+# binary search calculations
+# first divides the sorted list into half
+''' 
+iteration k = n/2^k
+          1 = n/2^k
+          k = n/2
+     log2 n = log2 2^k   
+     log2 n = k * log2 2
+          k = log n  -> O(log n)
 
 '''
