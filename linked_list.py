4e95c5639ef4d963e63d16a2ea7734b5c6c992c6
EthanBukenya <ethancylb@gmail.com>
2025-10-14 12:18:29 +0300

    added new linked list methods

--- linked_list.py
+++ linked_list.py
@@ -24,11 +24,79 @@ class LinkedList:
             itr = itr.next
         print(llstr)
 
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = Node(data, None)
+            return
+
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+        itr.next = Node(data, None)
+
+    def insert_values(self, data_list):
+        self.head = None
+        for data in data_list:
+            self.insert_at_end(data)
+
+    def get_length(self):
+        count = 0
+        itr = self.head
+        while itr:
+            count += 1
+            itr = itr.next
+
+        return count
+
+    def remove_at(self, index):
+        if index < 0 or index >= self.get_length():
+            raise Exception("invalid Index")
+
+        if index == 0:
+            self.head = self.head.next
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                itr.next = itr.next.next
+                break
+
+            itr = itr.next
+            count += 1
+
+    def insert_at(self, index, data):
+        if index < 0 or index >= self.get_length():
+            raise Exception("invalid index")
+
+        if index == 0:
+            self.insert_at_begining(data)
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                node = Node(data, itr.next)
+                itr.next = node
+                break
+
+            itr = itr.next
+            count += 1
+
 
 if __name__ == '__main__':
     ll = LinkedList()
-    ll.insert_at_begining(1)
-    ll.insert_at_begining(2)
-    ll.insert_at_begining(3)
-    ll.insert_at_begining(4)
+    # ll.insert_at_begining(1)
+    # ll.insert_at_begining(3)
+    # ll.insert_at_begining(5)
+    # ll.insert_at_begining(7)
+
+    # ll.insert_at_end(100)
+    ll.insert_values(['Apples', 'bananas', 'straw berry'])
+    print("length is : ",  ll.get_length())
+    # ll.remove_at(2)
+    ll.print()
+    ll.insert_at(1, "sorghum")
     ll.print()
