c4835d5f413bafeb6e2ae489a40b115bca8bc56a
EthanBukenya <ethancylb@gmail.com>
2025-10-15 10:11:44 +0300

    created hash_table.py

--- linked_list.py
+++ linked_list.py
@@ -117,6 +117,93 @@ class LinkedList:
 
             itr = itr.next
 
+# class Node:
+#     def __init__(self, data=None, next=None):
+#         self.data = data
+#         self.next = next
+
+
+# class LinkedList:
+#     def __init__(self):
+#         self.head = None
+
+#     def insert_at_begining(self, data):
+#         node = Node(data, self.head)
+#         self.head = node
+
+#     def print(self):
+#         if self.head is None:
+#             print("Link List is empty")
+#             return
+
+#         itr = self.head
+#         llstr = ''
+#         while itr:
+#             llstr += str(itr.data) + '-->'
+#             itr = itr.next
+#         print(llstr)
+
+#     def insert_at_end(self, data):
+#         if self.head is None:
+#             self.head = Node(data, None)
+#             return
+
+#         itr = self.head
+#         while itr.next:
+#             itr = itr.next
+#         itr.next = Node(data, None)
+
+#     def insert_values(self, data_list):
+#         self.head = None
+#         for data in data_list:
+#             self.insert_at_end(data)
+
+#     def get_length(self):
+#         count = 0
+#         itr = self.head
+#         while itr:
+#             count += 1
+#             itr = itr.next
+
+#         return count
+
+#     def remove_at(self, index):
+#         if index < 0 or index > self.get_length():
+#             raise Exception("Invalid index")
+
+#         if index == 0:
+#             self.head = self.head.next
+#             return
+
+#         itr = self.head
+#         count = 0
+#         while itr:
+#             if count == index - 1:
+#                 itr.next = itr.next.next
+#                 break
+
+#             itr = itr.next
+#             count += 1
+
+#     def insert_at(self, index, data):
+#         if index < 0 or index > self.get_length():
+#             raise Exception("Invalid index")
+
+#         if index == 0:
+#             self.insert_at_begining(data)
+#             return
+
+#         count = 0
+#         itr = self.head
+#         while itr:
+#             if count == index - 1:
+#                 node = Node(data, itr.next)
+#                 itr.next = node
+#                 break
+
+#             itr = itr.next
+#             count += 1
+
 
 if __name__ == '__main__':
     ll = LinkedList()
@@ -124,15 +211,18 @@ if __name__ == '__main__':
     # ll.insert_at_begining(3)
     # ll.insert_at_begining(5)
     # ll.insert_at_begining(7)
+    # ll.print()
 
     # ll.insert_at_end(100)
+    # ll.print()
     ll.insert_values(['Apples', 'bananas', 'straw berry'])
+    ll.print()
     print("length is : ",  ll.get_length())
-    # ll.remove_at(2)
+    ll.remove_at(2)
     ll.print()
-    ll.insert_at(1, "sorghum")
+    ll.insert_at(0, "sorghum")
     ll.print()
-    ll.insert_after_value("bananas", "kiwi")
-    ll.print()
-    ll.remove_by_value("sorghum")
-    ll.print
+    # ll.insert_after_value("bananas", "kiwi")
+    # ll.print()
+    # ll.remove_by_value("sorghum")
+    # ll.print
