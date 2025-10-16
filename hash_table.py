b5c79c8a31255e0bee38f36444fa7f95e956553d
EthanBukenya <ethancylb@gmail.com>
2025-10-16 10:44:47 +0300

    added collision handling by separate linking

--- hash_table.py
+++ hash_table.py
@@ -4,7 +4,7 @@
 class HashTable:
     def __init__(self):
         self.MAX = 100
-        self.arr = [None for i in range(self.MAX)]
+        self.arr = [[] for i in range(self.MAX)]
 
     def get_hash(self, key):
         h = 0
@@ -12,15 +12,39 @@ class HashTable:
             h += ord(char)
         return h % self.MAX
 
-    def add(self, key, val):
+    def __setitem__(self, key, val):
         h = self.get_hash(key)
-        self.arr[h] = val
 
-    def get(self, key):
-        h = self.get_hash(key)
+        found = False
+        for idx, element in enumerate(self.arr[h]):
+            if len(element) == 2 and element[0] == key:
+                self.arr[h][idx] = (key, val)
+                found = True
+                break
+        if not found:
+            self.arr[h].append(key, val)
+
+    def __getitem__(self, index):
+        h = self.get_hash(index)
         return self.arr[h]
 
+    def __delitem__(self, key):
+        h = self.get_hash(key)
+        self.arr[h] = None
 
-ht = HashTable()
-ht.add('march 7', 202)
-print(ht.get("march 7"))
+
+h = HashTable()
+
+print(h.get_hash('march 6'))
+print(h.get_hash('march 17'))
+
+h['march 6'] = 120
+h['march 7'] = 108
+h['march 17'] = 459
+h['march 9'] = 209
+h['march 8'] = 208
+
+print(h['march 6'])
+print(h['march 17'])
+# del h['march 7']
+# print(h.arr)
