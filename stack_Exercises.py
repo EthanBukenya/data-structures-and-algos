0dd953519fad2e6fa52f2aa4051132a16dfd7508
EthanBukenya <ethancylb@gmail.com>
2025-10-17 13:37:14 +0300

    updated stack_exercises

--- stack_Exercises.py
+++ stack_Exercises.py
@@ -11,3 +11,55 @@ is_balanced("((a+b))")     --> True
 is_balanced("))")          --> False
 is_balanced("[a+b]*(x+2y)*{gg+kk}") --> True
 '''
+from collections import deque
+
+
+class Stack:
+    def __init__(self):
+        self.container = deque()
+
+    def push(self, val):
+        self.container.append(val)
+
+    def pop(self):
+        return self.container.pop()
+
+    def peek(self):
+        return self.container[-1]
+
+    def is_empty(self):
+        return self.container == 0
+
+    def size(self):
+        return len(self.container)
+
+
+def reverse_string(s):
+    stack = Stack()
+    for char in s:
+        stack.push(char)
+
+    revstr = ''
+    while stack.size() != 0:
+        revstr += stack.pop()
+
+    return revstr
+
+
+s = Stack()
+s.push("1")
+s.push("2")
+s.push("3")
+s.push("4")
+s.push("5")
+
+
+print(s.peek())
+print(s.size())
+# print(s[::-1])
+print(reverse_string('hey you'))
+
+hello = "Hello"
+reverse_hello = hello[::-1]
+
+print(reverse_hello)
