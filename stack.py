f986e39674a9a37bdd1c59291e1c7d60b1c6d033
EthanBukenya <ethancylb@gmail.com>
2025-10-17 12:12:36 +0300

    created stack.py

---
+++ stack.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+
+class Stack:
+    def __init__(self):
+        self.container = deque()
+
+    def push(self, val):
+        self.container.append(val)
+
+    def pop(self):
+        return self.container.pop()
+
+    def peek(self):
+        return self.container[-1]
+
+    def is_empty(self):
+        return self.container == 0
+
+    def size(self):
+        return len(self.container)
+
+
+s = Stack()
+s.push("1")
+s.push("2")
+s.push("3")
+s.push("4")
+s.push("5")
+
+print(s.is_empty())
+
+s.pop()
+
+print(s.peek())
+print(s.size())
