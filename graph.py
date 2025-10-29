164a87f639249c1a21c83714001cb333b7d72757
EthanBukenya <ethancylb@gmail.com>
2025-10-29 11:57:48 +0300

    added shortest_path() method

--- graph.py
+++ graph.py
@@ -28,12 +28,43 @@ class Graph:
                     paths.append(p)
         return paths
 
+    def get_shortest_route(self, start, end, path=[]):
+        path = path + [start]
+
+        if start == end:
+            return path
+
+        if start not in self.graph_dict:
+            return None
+
+        shortest_path = None
+        for node in self.graph_dict[start]:
+            if node not in path:
+                sp = self.get_shortest_route(node, end, path)
+                if sp:
+                    if shortest_path is None or len(sp) < len(shortest_path):
+                        shortest_path = sp
+
+        return shortest_path
+
 
 if __name__ == '__main__':
     routes = [
         ("Kampala", "Nairobi"),
-        ("Kampala", "Dar Es Salaam"),
-        ("Entebbe", "Soroti")
+        ("Kampala", "Dodoma"),
+        ("Dodoma", "Nairobi"),
+        ("Dodoma", "Dar Es Salaam"),
+        ("Entebbe", "Soroti"),
+        ("Nairobi", "Dar Es Salaam"),
+        ("Kampala", "Dar Es Salaam")
     ]
 
     graph_routes = Graph(routes)
+    start = "Kampala"
+    end = "Dar Es Salaam"
+
+    print(f"graph routes between {start} and {end} are : ",
+          graph_routes.get_paths(start, end))
+
+    print(f"shortest routes between {start} and {end} are : ",
+          graph_routes.get_shortest_route(start, end))
