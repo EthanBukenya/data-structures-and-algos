57551841315e8ea13363f955d2420bef761e9f2d
EthanBukenya <ethancylb@gmail.com>
2025-11-04 11:06:15 +0300

    added binary_search_recurssive. method

--- algorithms/Binary_search.py
+++ algorithms/Binary_search.py
@@ -17,29 +17,6 @@
 #     'amount': '800'
 # }]
 
-
-# def find_transaction(transactions):
-#     for index, element in enumerate(transactions):
-#         if element['device_id'] == 'ldw47':
-#             return index
-#     return -1
-
-# def linear_search(numbers_list, number_to_find):
-#     for index, element in enumerate(numbers_list):
-#         if element == number_to_find:
-#             return index
-#     return 'infinity'
-
-
-# if __name__ == '__main__':
-
-#     numbers_list = [2, 4, 5, 6, 7, 10]
-#     number_to_find = 79
-#     index = linear_search(numbers_list, number_to_find)
-
-#     print(f"found number at index : {index}")
-
-
 def binary_search(numbers_list, number_to_find):
     left_index = 0
     right_index = len(numbers_list) - 1
@@ -60,10 +37,34 @@ def binary_search(numbers_list, number_to_find):
     return -1
 
 
+def binary_search_recurssive(numbers_list, number_to_find, left_index, right_index):
+    if right_index < left_index:
+        return -1
+
+    mid_index = (left_index + right_index) // 2
+    mid_number = numbers_list[mid_index]
+
+    if mid_number == number_to_find:
+        return mid_index
+
+    if mid_number < number_to_find:
+        left_index = mid_number + 1
+    else:
+        right_index = mid_number + 1
+
+    return binary_search_recurssive(numbers_list, number_to_find, left_index, right_index)
+
+
 if __name__ == '__main__':
+    # numbers_list = [1, 3, 5, 7, 9, 10]
+    # number_to_find = 7
+    # index = binary_search(numbers_list, number_to_find)
 
-    numbers_list = [2, 4, 5, 6, 7, 10]
-    number_to_find = 79
-    index = binary_search(numbers_list, number_to_find)
+    # print(f"the mid number index is {index} ")
 
-    print(f"found number at index : {index}")
+    numbers_list = [1, 3, 5, 7, 9, 10]
+    number_to_find = 7
+    index = binary_search_recurssive(
+        numbers_list, number_to_find, 0, len(numbers_list))
+
+    print(f"the mid number index is {index} ")
